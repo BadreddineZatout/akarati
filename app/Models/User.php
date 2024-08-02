c841f7afed0fc1bf581a1562975cdfe5754d9701
Mohammed Haida Mosbah <lampmohammed07@gmail.com>
2024-08-02 16:20:14 +0100

    users management (create soft-delete view index ) issue #5

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use BezhanSalleh\FilamentShield\Traits\HasPanelShield;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -11,7 +13,9 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use HasPanelShield;
     use HasRoles;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
