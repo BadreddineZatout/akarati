d35dada2e0bee9497f532571328fa380d7e2c9c0
BadreddineZatout <hb_zatout@esi.dz>
2024-08-02 18:09:41 +0100

    chore: db design

---
+++ database/migrations/2024_08_02_163640_create_promotions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('promotion_type_id')->constrained();
+            $table->foreignId('block_id')->constrained();
+            $table->string('state');
+            $table->double('selling_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
+};
