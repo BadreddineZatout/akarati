aeda1872337088d961db0971cf63eb08f7639b83
Mohammed Haida Mosbah <lampmohammed07@gmail.com>
2024-08-03 16:04:29 +0100

    feat/project management (create,edit,delete) issue #9

---
+++ app/Enums/ProjectStatusEnum.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enums;
+
+enum ProjectStatusEnum: string
+{
+    case PENDING = 'pending';
+    case NotLaunched = 'not_launched';
+    case InProgress = 'in_progress';
+    case Completed = 'completed';
+    case Archived = 'archived';
+
+    public static function color($value): string
+    {
+        return match ($value) {
+            self::PENDING->value => 'warning',
+            self::NotLaunched->value => 'danger',
+            self::InProgress->value => 'primary',
+            self::Completed->value => 'success',
+            self::Archived->value => 'secondary',
+        };
+    }
+}
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -18,5 +19,17 @@ class Project extends Model
     public function payments(): HasMany
     {
         return $this->hasMany(Payment::class);
+    }
+    public function promoter(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'promoter_id');
+    }
+    public function accountant(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'accountant_id');
+    }
+    public function blocks(): HasMany
+    {
+        return $this->hasMany(Block::class);
     }
 }
