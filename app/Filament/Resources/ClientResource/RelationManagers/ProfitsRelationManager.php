3baf8a6515b48b60ed3087c9d98e76e6be0658f3
BadreddineZatout <hb_zatout@esi.dz>
2024-08-10 15:23:58 +0100

    feat: update rest based on profit

--- app/Filament/Resources/ClientResource/RelationManagers/ProfitsRelationManager.php
+++ app/Filament/Resources/ClientResource/RelationManagers/ProfitsRelationManager.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\ClientResource\RelationManagers;
 
+use App\Enums\ProfitStateEnum;
+use App\Models\ClientPromotion;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
+use Filament\Notifications\Notification;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Filters\SelectFilter;
@@ -22,22 +25,26 @@ class ProfitsRelationManager extends RelationManager
             ->schema([
                 Forms\Components\Select::make('promotion_id')
                     ->label('Promotion')
-                    ->options(fn () => $this->ownerRecord->promotions()->get()->pluck('fullname', 'id'))
+                    ->options(fn () => $this->ownerRecord->promotions()->wherePivot('state', ProfitStateEnum::NOT_PAID->value)->get()->pluck('fullname', 'id'))
+                    ->required()
+                    ->hiddenOn('edit'),
+                Forms\Components\DatePicker::make('paid_at')
                     ->required(),
-                Forms\Components\DatePicker::make('paid_at'),
                 Forms\Components\Select::make('role')
                     ->live()
                     ->label('Role')
                     ->options(fn () => Role::whereNotIn('name', ['super_admin', 'panel_user'])->pluck('name', 'name'))
                     ->dehydrated(false)
-                    ->required(),
+                    ->required()
+                    ->hiddenOn('edit'),
                 Forms\Components\Select::make('paid_to')
                     ->required()
                     ->label('Paid To')
                     ->placeholder(fn (Get $get): string => empty($get('role')) ? 'First select role' : 'Select an option')
                     ->options(function (Get $get) {
                         return User::role($get('role'))->pluck('name', 'id');
-                    }),
+                    })
+                    ->hiddenOn('edit'),
                 Forms\Components\TextInput::make('amount')
                     ->required()
                     ->numeric(),
@@ -63,11 +70,82 @@ class ProfitsRelationManager extends RelationManager
                     ->options(fn () => $this->ownerRecord->promotions()->get()->pluck('fullname', 'id')),
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                Tables\Actions\CreateAction::make()
+                    ->before(function ($action, $data) {
+                        $promotion = ClientPromotion::where([
+                            'client_id' => $this->ownerRecord->id,
+                            'promotion_id' => $data['promotion_id'],
+                        ])->first();
+                        if ($data['amount'] > $promotion->rest) {
+                            Notification::make()
+                                ->danger()
+                                ->title('The amount is more than the rest of payment')
+                                ->send();
+
+                            $action->halt();
+                        }
+                    })
+                    ->after(function ($record) {
+                        $promotion = ClientPromotion::where([
+                            'client_id' => $this->ownerRecord->id,
+                            'promotion_id' => $record->promotion_id,
+                        ])->first();
+                        $promotion->decrement('rest', $record->amount);
+                        if ($promotion->rest <= 0) {
+                            if ($promotion->rest < 0) {
+                                $promotion->rest = 0;
+                            }
+                            $promotion->state = ProfitStateEnum::PAID->value;
+                        }
+                        $promotion->save();
+                    }),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->before(function ($action, $record, $data) {
+                        if ($record->amount != $data['amount']) {
+                            $promotion = ClientPromotion::where([
+                                'client_id' => $this->ownerRecord->id,
+                                'promotion_id' => $record->promotion_id,
+                            ])->first();
+
+                            $promotion->increment('rest', $record->amount);
+
+                            if ($data['amount'] > $promotion->rest) {
+                                Notification::make()
+                                    ->danger()
+                                    ->title('The amount is more than the rest of payment')
+                                    ->send();
+
+                                $action->halt();
+                            }
+                            $promotion->decrement('rest', $data['amount']);
+
+                            if ($promotion->state == ProfitStateEnum::PAID && $promotion->rest > 0) {
+                                $promotion->state = ProfitStateEnum::NOT_PAID;
+                            }
+
+                            if ($promotion->rest <= 0) {
+                                if ($promotion->rest < 0) {
+                                    $promotion->rest = 0;
+                                }
+                                $promotion->state = ProfitStateEnum::PAID->value;
+                            }
+                            $promotion->save();
+                        }
+                    }),
+                Tables\Actions\DeleteAction::make()
+                    ->after(function ($record) {
+                        $promotion = ClientPromotion::where([
+                            'client_id' => $this->ownerRecord->id,
+                            'promotion_id' => $record->promotion_id,
+                        ])->first();
+                        $promotion->increment('rest', $record->amount);
+                        if ($promotion->state == ProfitStateEnum::PAID) {
+                            $promotion->state = ProfitStateEnum::NOT_PAID->value;
+                        }
+                        $promotion->save();
+                    }),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
