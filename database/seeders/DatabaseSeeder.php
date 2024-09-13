1c3f4e23137e36c5a7049e4408c64c4215d9645f
BadreddineZatout <hb_zatout@esi.dz>
2024-09-13 15:39:19 +0100

    chore: plan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
 //            'name' => 'Test User',
 //            'email' => 'test@example.com',
 //        ]);
-//        $this->call(PlanSeeder::class);
+        $this->call(PlanSeeder::class);
         $this->call(ProjectLimitSeeder::class);
     }
 }
