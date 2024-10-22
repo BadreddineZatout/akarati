91e2bc48004c5aaee8e88acc4166bb9eeba82a5f
BadreddineZatout <hb_zatout@esi.dz>
2024-10-22 21:33:57 +0100

    feat: add check opn edit promotion price

--- app/Filament/Resources/PromotionResource/Pages/EditPromotion.php
+++ app/Filament/Resources/PromotionResource/Pages/EditPromotion.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\PromotionResource\Pages;
 
+use App\Enums\ProfitStateEnum;
 use App\Filament\Resources\BlockResource;
 use App\Filament\Resources\PromotionResource;
+use App\Models\ClientPromotion;
 use App\Models\Promotion;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPromotion extends EditRecord
@@ -29,6 +32,38 @@ class EditPromotion extends EditRecord
     protected function getRedirectUrl(): string
     {
         return PromotionResource::getUrl('promotions', ['record' => $this->record->block]);
+    }
+
+    protected function beforeSave(): void
+    {
+        if ($this->data['selling_price'] != $this->record->selling_price) {
+            $payment = ClientPromotion::where('promotion_id', $this->record->id)->first();
+            if ($payment) {
+                if ($payment->state = ProfitStateEnum::PAID) {
+                    Notification::make()
+                        ->danger()
+                        ->title('This promotion is already sold.')
+                        ->send();
+                    $this->halt();
+
+                    return;
+                }
+                if ($this->data['selling_price'] - $payment->rest < 0) {
+                    Notification::make()
+                        ->danger()
+                        ->title('The amount is invalid with the existing payments.')
+                        ->send();
+                    $this->halt();
+
+                    return;
+                }
+                $payment->rest = $this->data['selling_price'] - $payment->rest;
+                if ($payment->rest == 0) {
+                    $payment->state = ProfitStateEnum::PAID->value;
+                }
+                $payment->save();
+            }
+        }
     }
 
     public function getBreadcrumbs(): array
